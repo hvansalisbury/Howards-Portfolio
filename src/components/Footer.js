8021ab631c8e32ba23dcead410067defa58707c0
Howard Van Salisbury <vansal51@yahoo.com>
2023-03-09 10:00:53 -0500

    fixed images

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import githublogo from '../assets/images/github-mark.svg';
+import linkedinlogo from '../assets/images/iconmonstr-linkedin-3.svg';
+import youtubelogo from '../assets/images/youtube-svgrepo-com.svg';
 
 const styles = {
     footer: {
@@ -14,9 +17,9 @@ function Footer() {
 
     return (
         <footer style={styles.footer} className='d-flex justify-content-center align-items-center bg-secondary'>
-            <a className='m-2' href="https://github.com/hvansalisbury"><img src="github-mark.svg" alt="github-logo" height="50" width="50"></img></a>
-            <a className='m-2' href="https://www.linkedin.com/in/howard-salisbury-5a547998/"><img src="iconmonstr-linkedin-3.svg" alt="linkedin-logo" height="50" width="50"></img></a>
-            <a className='m-2' href="https://www.youtube.com/channel/UCtYCGStZXgJ9pHXe_r3B13w"><img src="youtube-svgrepo-com.svg" alt="youtube-logo" height="50" width="50"></img></a>
+            <a className='m-2' href="https://github.com/hvansalisbury"><img src="github-mark.svg" alt={githublogo} height="50" width="50"></img></a>
+            <a className='m-2' href="https://www.linkedin.com/in/howard-salisbury-5a547998/"><img src={linkedinlogo} alt="linkedin-logo" height="50" width="50"></img></a>
+            <a className='m-2' href="https://www.youtube.com/channel/UCtYCGStZXgJ9pHXe_r3B13w"><img src={youtubelogo} alt="youtube-logo" height="50" width="50"></img></a>
         </footer>
     )
 }
